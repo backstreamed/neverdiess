71d7041faaaf3382d5423b36da22d44e5376bc38
backstreamed <kursattt1963@gmail.com>
2026-03-23 21:28:13 +0300

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,14 +1,23 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
 loadstring(request({Url='https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua'; Method='GET'}).Body)();
 
 if queue_on_teleport then
     local code = [[
-    script_key = "]] .. script_key .. [["
-    loadstring(request({Url='https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua'; Method='GET'}).Body)();
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+task.wait(3)
 
-    if queue_on_teleport then
-        queue_on_teleport(neverdiess)
-    end
-    ]]
-    code = code:gsub("neverdiess", string.format("%q", code))
+script_key = "]] .. script_key .. [["
+loadstring(request({Url='https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua'; Method='GET'}).Body)();
+
+if queue_on_teleport then
+    queue_on_teleport(NEVERDIESS_PLACEHOLDER)
+end
+]]
+    code = code:gsub("NEVERDIESS_PLACEHOLDER", string.format("%q", code))
     queue_on_teleport(code)
 end
