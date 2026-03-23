377a726dc3fde1a3e78c425d3b9190a04126f2df
backstreamed <kursattt1963@gmail.com>
2026-03-24 01:01:54 +0300

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,15 +1,25 @@
 local LOADER = "https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua"
-local SELF_URL = "https://raw.githubusercontent.com/backstreamed/neverdiess/refs/heads/main/mainloader.lua"
+local SELF_URL = "https://raw.githubusercontent.com/backstreamed/neverdiess/main/mainloader.lua"
 
 if not neverdiess_key then
-    warn("not found")
+    warn("neverdiess_key tanımlanmamış!")
     return
 end
 
-local queueteleport = queue_on_teleport or (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
+-- Queue fonksiyonunu bul
+local queueteleport = (type(queue_on_teleport) == "function" and queue_on_teleport)
+    or (type(syn) == "table" and syn.queue_on_teleport)
+    or (type(fluxus) == "table" and fluxus.queue_on_teleport)
 
 if queueteleport then
-    queueteleport('neverdiess_key = "' .. neverdiess_key .. '" repeat task.wait() until game:IsLoaded() repeat task.wait() until game.Players.LocalPlayer loadstring(game:HttpGet("' .. SELF_URL .. '"))()')
+    local queueCode = 'neverdiess_key = "' .. tostring(neverdiess_key) .. '"\n'
+        .. 'repeat task.wait() until game:IsLoaded()\n'
+        .. 'repeat task.wait() until game.Players.LocalPlayer\n'
+        .. 'repeat task.wait() until game.Players.LocalPlayer.Character\n'
+        .. 'task.wait(2)\n'
+        .. 'loadstring(game:HttpGet("' .. SELF_URL .. '"))()'
+
+    queueteleport(queueCode)
 end
 
 if not game:IsLoaded() then
