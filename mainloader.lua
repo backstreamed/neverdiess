1ace1880a27a0e68ae115d337bd9a7c041f92708
backstreamed <kursattt1963@gmail.com>
2026-03-23 21:14:22 +0300

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,14 +1,27 @@
-loadstring(request({Url='https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua'; Method='GET'}).Body)();
+request = request or http_request or syn.request or http.request or function(a) return {Body = game:HttpGet(a.Url)} end
+
+loadstring(request({
+    Url = 'https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua',
+    Method = 'GET'
+}).Body)()
+
 
 if queue_on_teleport then
-    local code = [[
-        script_key = "]] .. script_key .. [["
-        loadstring(request({Url='https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua'; Method='GET'}).Body)();
+    
+    local code = string.format([[
+        request = request or http_request or syn.request or http.request or function(a) return {Body = game:HttpGet(a.Url)} end
+
+        script_key = %q
+
+        loadstring(request({
+            Url = 'https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua',
+            Method = 'GET'
+        }).Body)()
 
         if queue_on_teleport then
-            queue_on_teleport(src)
+            queue_on_teleport(debug.getinfo(1).source:sub(2))
         end
-        ]]
-    code = code:gsub("src", string.format("%q", code))
+    ]], script_key)
+
     queue_on_teleport(code)
 end
