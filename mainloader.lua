5a42b4cb059772917a51e240b5598527604376eb
backstreamed <kursattt1963@gmail.com>
2026-03-23 21:42:51 +0300

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,12 +1,16 @@
 local k = script_key or _G.script_key
 if not game:IsLoaded() then game.Loaded:Wait() end
-local r = request or http_request or syn.request
+local r = request or http_request or (syn and syn.request)
+
 loadstring(r({Url='https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua',Method='GET'}).Body)()
 if queue_on_teleport then
-    queue_on_teleport([[
-        script_key = "]] .. k .. [["
-        if not game:IsLoaded() then game.Loaded:Wait() end
-        local r = request or http_request or syn.request
+    local s = string.format([[
+        repeat task.wait() until game:IsLoaded()
+        task.wait(2)
+        _G.script_key = %q
+        script_key = %q
+        local r = request or http_request or (syn and syn.request)
         loadstring(r({Url='https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua',Method='GET'}).Body)()
-    ]])
+    ]], k, k)
+    queue_on_teleport(s)
 end
