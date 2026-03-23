4f92b74c0ab3920a823a904322c0883aea2fcfe9
backstreamed <kursattt1963@gmail.com>
2026-03-24 00:53:47 +0300

    Update mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -2,9 +2,20 @@ if queue_on_teleport then
     queue_on_teleport(string.format([[
         repeat task.wait() until game:IsLoaded()
         repeat task.wait() until game.Players.LocalPlayer
-        script_key="%s"
+        
+        script_key = "%s"
+        
+        if queue_on_teleport then
+            queue_on_teleport(string.format([[
+                repeat task.wait() until game:IsLoaded()
+                repeat task.wait() until game.Players.LocalPlayer
+                script_key = "%%s"
+                loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua"))()
+            ]], "%s"))
+        end
+        
         loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua"))()
-    ]], neverdiess))
+    ]], neverdiess_key, neverdiess_key))
 end
 
 if not game:IsLoaded() then
@@ -13,5 +24,5 @@ end
 repeat task.wait() until game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer.Character
 
-script_key = neverdiess
+script_key = neverdiess_key
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/6fa9dda5649accff2d9cc2ff8d5dadaa.lua"))()
